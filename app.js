5ef58666fc59e4d15c2cf645c8a2815301c0d0ed
Alice <Your emailalice.boulaypaw@hotmail.fr>
2023-01-17 15:35:58 +0100

    maj first commit

--- app.js
+++ app.js
@@ -1,123 +1,144 @@
 require('./models/connection');
+const { bg, bullet } = require('ervy');
 const Article = require('./models/articles');
 const User = require('./models/users');
 const Order = require('./models/orders');
 
-
 /*
-** Articles
-*/
+ ** Articles
+ */
 
 function displayAllArticles() {
-	Article.find().then(data => {
-		console.log('ARTICLES =>', data);
-	});
+  Article.find().then((data) => {
+    // console.log('ARTICLES =>', data);
+
+    const dataTable = [];
+    for (const article of data) {
+      if (article.stock > 5) {
+        dataTable.push({
+          key: article.name,
+          value: article.stock,
+          style: bg('blue'),
+          height: 30,
+        });
+      } else {
+        dataTable.push({
+          key: article.name,
+          value: article.stock,
+          style: bg('red'),
+          height: 30,
+        });
+      }
+    }
+
+    console.log(bullet(dataTable));
+  });
 }
-// Sample call:
-// displayAllArticles();
+displayAllArticles();
 
 function displayArticleByName(articleName) {
-	Article.findOne({ name: articleName }).then(data => {
-		console.log('ARTICLE =>', data);
-	});
+  Article.findOne({ name: articleName }).then((data) => {
+    console.log('ARTICLE =>', data);
+  });
 }
-// Sample call:
-// displayArticleByName('Weebot');
 
 function displayArticleByID(articleId) {
-	Article.findById(articleId).then(data => {
-		console.log('ARTICLE =>', data);
-	});
+  Article.finArticleyId(articleId).then((data) => {
+    console.log('ARTICLE =>', data);
+  });
 }
-// Sample call:
-// displayArticleByID('625ea9ca38d6a3776994651f');
 
 function updateArticlePrice(articleId, newPrice) {
-	Article.updateOne({ _id: articleId }, { price: newPrice }).then(() => {
-		console.log(`Price updated for ${articleId}`);
-	});
+  Article.updateOne({ _id: articleId }, { price: newPrice }).then(
+    () => {
+      console.log(`Price updated for ${articleId}`);
+    }
+  );
 }
-// Sample call:
-// updateArticlePrice('625ea9ca38d6a3776994651e', 149);
 
 function updateArticleStock(articleId, newStock) {
-	Article.updateOne({ _id: articleId }, { stock: newStock }).then(() => {
-		console.log(`Stock updated for ${articleId}`);
-	});
+  Article.updateOne({ _id: articleId }, { stock: newStock }).then(
+    () => {
+      console.log(`Stock updated for ${articleId}`);
+    }
+  );
 }
-// Sample call:
-// updateArticleStock('625ea9ca38d6a3776994651e', 14);
 
 function resetStocks() {
-	Article.updateMany({}, { stock: 0 }).then(() => {
-		console.log(`Stocks successfully reset`);
-	});
+  Article.updateMany({}, { stock: 0 }).then(() => {
+    console.log(`Stocks successfully reset`);
+  });
 }
-// Sample call:
-// resetStocks();
-
 
 /*
-** Users
-*/
+ ** Users
+ */
 
 function displayAllUsers() {
-	User.find().then(data => {
-		console.log('USERS =>', data);
-	});
+  User.find().then((data) => {
+    console.log('USERS =>', data);
+  });
 }
-// Sample call:
-// displayAllUsers();
 
 function deleteUser(userId) {
-	User.deleteOne({ _id: userId }).then(() => {
-		console.log(`User ${userId} deleted`);
-	});
+  User.deleteOne({ _id: userId }).then(() => {
+    console.log(`User ${userId} deleted`);
+  });
 }
-// Sample call:
-// deleteUser('625ea9ca38d6a3776994651j');
-
 
 /*
-** Orders
-*/
+ ** Orders
+ */
 
 function displayAllOrders() {
-	Order.find().then(data => {
-		console.log('ORDERS =>', data);
-	});
+  Order.find().then((data) => {
+    console.log('ORDERS =>', data);
+  });
 }
-// Sample call:
-// displayAllOrders();
 
 function updateOrderPaymentStatus(orderId, isPaid) {
-	Order.updateOne({ _id: orderId }, { isPaid }).then(() => {
-		console.log(`Order ${orderId} updated`);
-	});
+  Order.updateOne({ _id: orderId }, { isPaid }).then(() => {
+    console.log(`Order ${orderId} updated`);
+  });
 }
-// Sample call:
-// updateOrderPaymentStatus('625ea9ca38d6a37769946513', true);
 
 function deleteOrder(orderId) {
-	Order.deleteOne({ _id: orderId }).then(() => {
-		console.log(`Order ${orderId} deleted`);
-	});
+  Order.deleteOne({ _id: orderId }).then(() => {
+    console.log(`Order ${orderId} deleted`);
+  });
 }
-// Sample call:
-// deleteOrder('625ea9ca38d6a37769946515');
 
+/*
+ ** Query population
+ */
+
+function displayOrderArticles(orderId) {
+  Order.findById(orderId)
+    .populate('articles')
+    .then((data) => {
+      console.log('ORDER =>', data);
+    });
+}
+
+function displayUserOrders(userId) {
+  Order.find({ user: userId }).then((data) => {
+    console.log('ORDERS =>', data);
+  });
+}
 
 // Do not edit/remove code under this line
 module.exports = {
-	displayAllArticles,
-	displayArticleByName,
-	displayArticleByID,
-	updateArticlePrice,
-	updateArticleStock,
-	resetStocks,
-	displayAllUsers,
-	deleteUser,
-	displayAllOrders,
-	updateOrderPaymentStatus,
-	deleteOrder,
+  displayAllArticles,
+  displayArticleByName,
+  displayArticleByID,
+  updateArticlePrice,
+  updateArticleStock,
+  resetStocks,
+  displayAllUsers,
+  deleteUser,
+  displayAllOrders,
+  updateOrderPaymentStatus,
+  deleteOrder,
+  displayOrderArticles,
+  displayUserOrders,
 };
